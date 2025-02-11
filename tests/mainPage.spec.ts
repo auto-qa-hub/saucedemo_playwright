fcc24d6f7b3cb1a8fee3bee868edab7a59eda8fa
Andry-Ks <127143251+Andry-Ks@users.noreply.github.com>
2025-02-12 00:47:14 +0200

    added tests for main page

--- tests/mainPage.spec.ts
+++ tests/mainPage.spec.ts
@@ -6,5 +6,20 @@ test.describe("Main Page tests", () => {
         const mainPage = new MainPage(page);
         await mainPage.visitMainPage();
 
+    //Перевірка, що поле "Username" відображається
+    await expect(page.locator('input[data-test="username"]')).toBeVisible();
+
+    //Перевірка, що поле "Password" відображається
+    await expect(page.locator('input[data-test="password"]')).toBeVisible();
+
+    //Перевірка, що кнопка "Login" відображається і активна
+    await expect(page.locator('input[data-test="login-button"]')).toBeVisible();
+    await expect(page.locator('input[data-test="login-button"]')).toBeEnabled();
+
+    //Перевірка, що список користувачів відображається
+    await expect(page.locator('div[data-test="login-credentials"]')).toBeVisible();
+
+    //Перевірка, що блок з паролем відображається
+    await expect(page.locator('div[data-test="login-password"]')).toBeVisible();
     });
 });
